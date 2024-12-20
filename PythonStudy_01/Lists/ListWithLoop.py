a2ecf3ccfea4823465f0df66c2c3a40310646895
Lumi <drakko_games@outlook.com>
2024-12-20 17:02:06 -0300

    Trying to list looping
    I concluded that using List as a indexator AND as a class object for the list itself was not a very good idea.

---
+++ PythonStudy_01/Lists/ListWithLoop.py
@@ -0,0 +1,14 @@
+from itertools import count
+
+numbersList = [1, 2, 3, 4, 5]
+for y in range(len(numbersList)):
+    print(numbersList[y])
+    y += 1
+    print(y)
+    while y < len(numbersList) and y < 100:
+        print(list)
+        y += 1
+        if y == 100:
+            break
+        else:
+            continue
