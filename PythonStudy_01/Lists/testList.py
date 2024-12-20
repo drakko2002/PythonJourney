2f22e6715cd74bd882ff5ea4ed3104c36188a6ae
Lumi <drakko_games@outlook.com>
2024-12-20 18:30:17 -0300

    update - loop while is better
    o loop while não funcionou como eu esperava, mas funcionou melhor do que o looping for.

--- PythonStudy_01/Lists/testList.py
+++ PythonStudy_01/Lists/testList.py
@@ -1,7 +1,6 @@
 numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-for numero in numeros:
-    print(numero)
-    if numero < 50:
-        numeros.append((numero+1))
-    elif numero == 50:
+while numeros[-1] < 50: #O último elemento da lista pode ser alcançado com um simples -1
+    numeros.append(numeros[-1] + 1)
+    print(numeros)
+    if numeros[-1] == 50:
         break
