4906807cea9ecc622781b9124a5775214039292c
Lumi <drakko_games@outlook.com>
2024-12-20 18:35:54 -0300

    Finished the CounterList prototype
    I think i might be closer to learn BubbleSort method than i tought, and i may also achieve the crawler i'm working on in about to 2 months. Might also help Victor in this mean time.

--- PythonStudy_01/Lists/testList.py
+++ PythonStudy_01/Lists/testList.py
@@ -1,6 +1,9 @@
 numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 while numeros[-1] < 50: #O último elemento da lista pode ser alcançado com um simples -1
     numeros.append(numeros[-1] + 1)
-    print(numeros)
+    #    print(numeros) Isso conta todas as iterações da lista até chegar no 50.
     if numeros[-1] == 50:
+        print(numeros) # Então é melhor colocar a chamada dessa função dentro de uma condição de parada.
         break
+        #Ao ser atingida a condição de parada, ele vai printar a lista agora com todos os elementos,
+        #e ai sim vai finalizar o código.
