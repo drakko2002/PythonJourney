cecb96c9d00184eac9dc469a609f2ddc8e66a4fc
Lumi <drakko_games@outlook.com>
2024-12-23 11:41:02 -0300

    conditional with user input
    user can input a number and check if it is true.

---
+++ PythonStudy_01/bitMore/conditionals/otherBlocks.py
@@ -0,0 +1,16 @@
+x = int(input("Insira o número: "))
+if x < 10:
+    print("x is less than 10")
+elif x < 20:
+    print("x is less than 20")
+elif x > 20:
+    print("x is greater than 20")
+elif x == 20:
+    print("x is 20")
+    print("x is equal to 20")
+elif x == 5:
+    print("You entered 5")
+else:
+    print("X is not much bigger than 5.")
+
+    #Usuário pode inserir entrada de dados e esperar pelo prompt.
