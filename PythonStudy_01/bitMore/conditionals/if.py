82f1e282348a1c7636e011edb370eaf503b0ee61
Lumi <drakko_games@outlook.com>
2024-12-23 11:06:25 -0300

    created if.py
    Studying a bit more about conditionals to see if i'm not forgetting anything.

---
+++ PythonStudy_01/bitMore/conditionals/if.py
@@ -0,0 +1,11 @@
+condicion = str(input("Type something:"))
+if condicion == "Hello":
+    print("You typed Hello")
+elif condicion == "Bye":
+    print("You typed Bye")
+    print("Goodbye")
+elif condicion == "Hi":
+    print("You typed Hi")
+else:
+    print("You did not type Hello or Bye")
+
