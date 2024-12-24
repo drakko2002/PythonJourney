b596f4fd76eaeee49569ebb3c4212e51ff75ed85
Lumi <drakko_games@outlook.com>
2024-12-24 08:22:56 -0300

    updated again.py
    Implemented a While True loop with a Try/Except block to prevent user from inserting wrong information. (expected information is integer and not string.)

--- PythonStudy_01/redoTemp/datetime/again.py
+++ PythonStudy_01/redoTemp/datetime/again.py
@@ -1,8 +1,15 @@
 from datetime import datetime
-anoNascimento = int(input("Digite o ano de nascimento: "))
+def ano_nascimento():
+    while True:
+        try:
+            _anoNascimento = int(input("Digite o ano de nascimento: "))
+            return _anoNascimento
+        except ValueError:
+            print("Digite apenas números!")
+            continue
 def ano_atual() -> int:
     now = datetime.now()
     _ano_atual = now.year
     return int(_ano_atual)
-calc = ano_atual() - anoNascimento
+calc = ano_atual() - ano_nascimento()
 print(calc)
