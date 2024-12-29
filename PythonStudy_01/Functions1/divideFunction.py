4ae3683185063c3166d09ca7a45c87e7aea21392
Lumi <drakko_games@outlook.com>
2024-12-29 20:12:06 -0300

    Update divideFunction.py
    Studying about built-in fnctions with an better exception complement in Python.

--- PythonStudy_01/Functions1/divideFunction.py
+++ PythonStudy_01/Functions1/divideFunction.py
@@ -1,23 +1,23 @@
 def divide():
     try:
-        a = float(input("Digite o numerador: "))
-        b = float(input("Digite o denominador: "))
-        division = a / b
-        print(division)
+        a = float(input("Digite o numerador: ")) #Pede para usuário inserir dados A.
+        b = float(input("Digite o denominador: ")) #Pede para usuário inserir dados B.
+        division = a / b #Tenta realizar operação de divisão com A e B fornecidos.
+        print(division) #Imprime o resultado da Divisão na tela, se for bem-sucedida.
 
-    except ZeroDivisionError as error:
-        print(error)
+    except ZeroDivisionError as error: #Chama pela exceção ZeroDivision da classe Error,
+        print(error) #Imprime detalhes do erro.
         print("Impossível dividir por zero.")
 
 
-    except ValueError as error:
-        print(error)
+    except ValueError as error: #Chama pela exceção ValueError da classe Error.
+        print(error) #Imprime detalhes do erro.
         print("Digite um valor válido.")
 
 
-    except KeyboardInterrupt as error:
+    except KeyboardInterrupt as error: #Chama pela Exceção KeyboardInterrupt.
         print(error)
-        raise SystemExit(f"Usuário optou por finalizar programa. {error}")
+        raise SystemExit(f"Usuário optou por finalizar programa. {error}") from error
 
 
 divide() #Chama pela função divide.
