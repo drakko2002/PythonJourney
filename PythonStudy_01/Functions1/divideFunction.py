3a9c2f6c8044c783f4a38aff0f43063b70627c97
Lumi <drakko_games@outlook.com>
2024-12-30 14:09:40 -0300

    Update on divideFunction
    Better documentation for what every code part do.

--- PythonStudy_01/Functions1/divideFunction.py
+++ PythonStudy_01/Functions1/divideFunction.py
@@ -14,10 +14,12 @@ def divide():
         print(error) #Imprime detalhes do erro.
         print("Digite um valor válido.")
 
-
-    except KeyboardInterrupt as error: #Chama pela Exceção KeyboardInterrupt.
+    except KeyboardInterrupt as error: #Chama pela Exceção KeyboardInterrupt e inicia tratamento.
         print(error)
         raise SystemExit(f"Usuário optou por finalizar programa. {error}") from error
-
-
+'''Perceba que ao apagar a exceção de tratamento de erro KeyboardInterrupt, ele volta a
+retornar mensagens de erro não tratadas.'''
+#Quando o usuário finaliza o programa pelo atalho CTRL+C, ocorre um erro vermelho.
+#Aqui, essa exceção é tratada ao causar uma outra exceção para saída do sistema.
+#A saída de sistema vai imprimir o erro designado para a exceção SystemExit ao ser tratada.
 divide() #Chama pela função divide.
