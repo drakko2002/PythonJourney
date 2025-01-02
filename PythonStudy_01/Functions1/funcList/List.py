80c2d518f6b6240ece825e412bf89f5bb0b03c07
Lumi <drakko_games@outlook.com>
2025-01-02 19:53:12 -0300

    i don't remember

--- PythonStudy_01/Functions1/funcList/List.py
+++ PythonStudy_01/Functions1/funcList/List.py
@@ -1,5 +1,15 @@
 #list = range(1, 100)
+#from PythonStudy_01.bitMore.dict.nameDict import count
 
 #for num in list:
 #    print(num) #Com a palavra reservada FOR vc consegue dar nome para os elementos na lista
-    #Tornando mais fácil iterar pelos seus elementos.
+    #Tornando mais fácil iterar os seus elementos.
+FruitList = ["Apple","Banana", "Orange","Peach","Pear","Pineapple","Watermelon","Mango"]
+for fruit in FruitList: #FOR também é um condicional de loop
+    #Ou seja, enquanto o parâmetro estiver dentro do bloco FOR, ele continuará a se repetir.
+    #print(len(fruit)) #Isso só conta a quantidade de letras em cada elemento.
+    '''Mas é bom saber disso.'''
+    #print(fruit.upper()) #Imprime elementos todos em Uppercase
+    #print(fruit.lower()) #Imprime elementos em Lowercase
+    #print(FruitList.index(fruit)) #Indexa o número de elementos na lista.
+print(len)
