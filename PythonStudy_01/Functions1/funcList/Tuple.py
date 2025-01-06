0caa4955dc6e7909aa2ff0ef60fe6e392ff0dfb4
Lumi <drakko_games@outlook.com>
2025-01-06 15:37:44 -0300

    Updated Tuple.py
    The tuple now gets updated every time the user insert a element inside it or remove an element from it. By redeclaring the tuple var with numbers =  add_tuple(numbers) we can assure that the tuple will be updated and concateneted with the secondary tuple created by the function.

--- PythonStudy_01/Functions1/funcList/Tuple.py
+++ PythonStudy_01/Functions1/funcList/Tuple.py
@@ -5,7 +5,6 @@ Calcule e exiba:
 - A soma de todos os elementos.
 - O maior e o menor valor.
 - A posição do maior número na tupla.'''
-numbers: tuple = (1, 2, 3, 4, 5)
 #I decided to typecast the tuple.
 def sum_tuple(numbers: tuple) -> int: #Indica que o retorno esperado é INT
     return sum(numbers)
@@ -23,7 +22,19 @@ def position_tuple(numbers: tuple) -> int:
 def highest_tuple(numbers: tuple) -> int:
     return numbers[numbers.index(max(numbers))]
 
+def add_tuple(numbers: tuple) -> tuple:
+    num = int(input("Digite elemento para adicionar"))
+    return numbers + (num,)
+
+def del_tuple(numbers: tuple) -> tuple:
+    return numbers[:-1]
+
+def view_tuple(numbers: tuple) -> tuple:
+    return numbers
+
+
 def main():
+    numbers: tuple = (1, 2, 3, 4, 5)
     while True:
         print("-->Bem vindo ao Menu de Teste da Tupla <--")
         print("1 - Somar Tupla")
@@ -32,7 +43,10 @@ def main():
         print("4 - Maior valor da tupla, apenas.")
         print("5 - Posição do maior valor da tupla.")
         print("6 - Quantidade de elementos da tupla.")
-        print("7 - Sair")
+        print("7 - Adicionar elemento a tupla.")
+        print("8 - Remover elemento a tupla.")
+        print("9 - Ver elementos da tupla.")
+        print("10 - Sair")
         try:
             opcao_menu = int(input("Digite a opção desejada: "))
             if opcao_menu == 1:
@@ -66,16 +80,35 @@ def main():
                 #A única razão pra isso funcionar, é a função print que executa algo independente.
                 break
 
-                
             elif opcao_menu == 7:
+                numbers = add_tuple(numbers)
+                print(f"Tupla atualizada: {numbers}")
+
+
+
+
+            elif opcao_menu == 8:
+                numbers = del_tuple(numbers)
+                print(f"Tupla atualizada: {numbers}")
+
+            elif opcao_menu == 9:
+                print(view_tuple(numbers))
+
+            elif opcao_menu == 10:
                 raise SystemExit("Programa encerrado.")
 
 
-        except (ValueError):
-            print("Digite uma opção válida.")
+        except (ValueError, TypeError):
+            print("Digite um valor válido.")
             continue
         except (KeyboardInterrupt):
             print("Programa encerrado.")
             break
 #Cant forget to call the main() function to start the flux of the program.
 main()
+#Due to Tuples inate imutability, it's imposible to direct add a element and delete a element
+#From it. Althought we can still create another tuple which can replace the current tuple
+#elements.
+####################
+#Also, by re-declaring the tuple with the respective function willing to modify it's elements
+#It will also update the tuple with the inserted values.
