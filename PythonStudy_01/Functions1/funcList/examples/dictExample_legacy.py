24700c15f61659e2563cd3acf1fe2c51f0cce198
Lumi <drakko_games@outlook.com>
2025-01-06 13:35:20 -0300

    Update dictExample_legacy.py
    The variable are now collected inside the respective functions, and not globally set.
     - Now the main flow breaks when the desired action is finnished and return to the menu.
    - There is no more infinite looping.

--- PythonStudy_01/Functions1/funcList/examples/dictExample_legacy.py
+++ PythonStudy_01/Functions1/funcList/examples/dictExample_legacy.py
@@ -1,6 +1,5 @@
 dicionarioTeste = {"ex1": 1, "ex2": 2, "ex3": 3}
 
-
 #Objetivo do programa: Criar dicionário com 3 chaves
 #Permitir que o usuário adicione, remova e visualize as chaves no fluxo principal
 #Ao realizar uma dessas operações, ele deverá retornar um dicionário atualizado.
@@ -15,19 +14,15 @@ dicionarioTeste = {"ex1": 1, "ex2": 2, "ex3": 3}
 def valida_opcao():
     while True:
         try:
-            opcao = int(input("Digite a opção desejada: "))
-            acao_chave = input("Digite a chave: ")
-            valor_chave = int(input("Digite o valor da chave: "))
+            opcao = int(input("Digite opção desejada: "))
             if opcao not in [1,2,3,4]:
                 print("Suas opções são 1, 2, 3 e 4.")
-                #Ao invocar Raise ValueError aqui, o código finaliza por erro de valor.
-                #Isso torna o "continue" do bloco inalcançável.
                 continue
             if opcao == 1:
                 print("Selecionou atualizar chave.")
                 try:
-                    print(acao_chave)
-                    print(valor_chave)
+                    acao_chave = input("Digite a chave: ")
+                    valor_chave = int(input("Digite o valor da chave: "))
 
                     print("Chave atualizada!")
 
@@ -38,35 +33,34 @@ def valida_opcao():
                     print("Algo aconteceu. Tente novamente.")
                     continue
             if opcao == 2:
+                acao_chave = input("Digite a chave que deseja excluir: ")
                 print("Selecionou excluir chave.")
                 print(acao_chave)
-                while True:
-                    try:
-                        if acao_chave in dicionarioTeste:
-                            dicionarioTeste.pop(acao_chave)
-                            continue
-                        if acao_chave not in dicionarioTeste:
-                            print("O nome da chave não existe.")
-                            return
-                        if valor_chave not in dicionarioTeste:
-                            print("O valor da chave não existe.")
-                            return
-                    except(KeyError,IndexError,ValueError) as error:
-                        print(error)
-                        print("Algo aconteceu. Tente novamente.")
-                        continue
+
+                if acao_chave in dicionarioTeste:
+                    dicionarioTeste.pop(acao_chave)
+                    print(dicionarioTeste)
+                    return
+                #Retorna versão atualizada do dicionário.
+                else:
+                    raise ValueError("Chave não existe.")
+
             if opcao == 3:
                 print("Selecionou visualizar chave.")
+                acao_chave = input("Digite a chave que deseja visualizar: ")
                 print(acao_chave)
                 if acao_chave in dicionarioTeste:
                     print(dicionarioTeste[acao_chave])
-                    break
+                    return dicionarioTeste
+                #retorna versão atualizada do dicionário
                 if acao_chave not in dicionarioTeste:
                     print("A chave não existe.")
                     continue
             if opcao == 4:
                 print("Saindo do programa...")
                 raise SystemExit("Fluxo do programa foi finalizado.")
+
+            #Sessão de tratamento de erros.
         except ValueError:
             print("O valor digitado está incorreto. Tente novamente.")
             continue
