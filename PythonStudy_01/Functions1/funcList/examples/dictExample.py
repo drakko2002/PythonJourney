2e83922f55e894cc9da0e595242cedbcd5c5d151
Lumi <drakko_games@outlook.com>
2025-01-06 00:21:04 -0300

    Update dictExample.py

--- PythonStudy_01/Functions1/funcList/examples/dictExample.py
+++ PythonStudy_01/Functions1/funcList/examples/dictExample.py
@@ -2,27 +2,49 @@ dicionarioTeste = {"ex1": 1, "ex2": 2, "ex3": 3}
 print(dicionarioTeste)
 
 
-def valida_opcao(opcao):
-    opcao = [1, 2, 3, 4, 5]
-    valida_entrada = int(input("Digite uma opção entre 1 e 5: "))
-    while valida_entrada not in opcao:
+#Objetivo do programa: Criar dicionário com 3 chaves
+#Permitir que o usuário adicione, remova e visualize as chaves no fluxo principal
+#Ao realizar uma dessas operações, ele deverá retornar um dicionário atualizado.
+
+
+def valida_opcao():
+
+    while valida_entrada not in:
         try:
+            valida_entrada = int(input("Digite uma opção entre 1 e 5: "))
             print(valida_entrada)
             if valida_entrada not in opcao:
                 raise ValueError
+            if valida_entrada in opcao:
+                break
         except ValueError:
             print("A opção inserida não existe.")
-            valida_opcao(opcao)
+            valida_opcao()
+
+
+valida_opcao()
 
 
 def fluxo():
     while True:
-        print("--> Menu do Dicionário <--")
-        print("1 - Adicionar chave")
-        print("2 - Modificar chave")
-        print("3 - Excluir chave")
-        print("4 - Visualizar chave")
-        print("5 - Sair")
+        try:
+            print("--> Menu do Dicionário <--")
+            print("1 - Adicionar chave")
+            print("2 - Modificar chave")
+            print("3 - Excluir chave")
+            print("4 - Visualizar chave")
+            print("5 - Sair")
+            valida_opcao()
+        except ValueError:
+            print("Opção inválida.")
+            fluxo()
+        except KeyboardInterrupt:
+            print("Programa encerrado.")
+            break
+        except SystemExit:
+            print("Programa encerrado.")
+            break
+
 
 '''        valida_chave = str(input("Digite uma chave: "))
         modifica_chave = int(input("Digite um valor: "))
