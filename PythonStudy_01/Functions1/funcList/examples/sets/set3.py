214e51ee96990e0adb54f15e38fa7a5d8a819569
Lumi <drakko_games@outlook.com>
2025-01-09 19:12:31 -0300

    setting next steps

---
+++ PythonStudy_01/Functions1/funcList/examples/sets/set3.py
@@ -0,0 +1,4 @@
+#Todo: Fazer gestor de coleção de filmes.
+#Todo: Descrição: Crie um programa que permita ao usuário adicionar, remover e listar filmes em uma coleção, garantindo que nenhum filme seja repetido.
+#Todo: Objetivo: Aprender a adicionar (add) e remover (remove) elementos de um set.
+#Todo: Desafio Extra: Adicione funcionalidade para verificar se um filme já está na coleção.
---
+++ PythonStudy_01/Functions1/funcList/examples/sets/set4.py
@@ -0,0 +1,4 @@
+#Todo: Descrição: Faça um programa que permita verificar quem esteve presente em uma reunião
+# com base em duas listas diferentes de presença.
+#Todo: Objetivo: Praticar operações como união, interseção e diferença de sets.
+#Todo: Desafio Extra: Adicione um terceiro grupo e analise quem compareceu em todos os encontros.
