2a50218eb570120ee1e84f491cd77d26ed5f4e93
Lumi <drakko_games@outlook.com>
2025-01-09 14:27:48 -0300

    Update estoqueDict.py

--- PythonStudy_01/Functions1/funcList/examples/simulaEstoque/estoqueDict.py
+++ PythonStudy_01/Functions1/funcList/examples/simulaEstoque/estoqueDict.py
@@ -19,7 +19,9 @@ def estoque_dict(estoque:dict=(str,int)) -> dict:
     while True:
         try:
             #Validação da entrada do produto do usuário.
+            print("-> Digite 'sair' para voltar ao menu <-")
             produto = str(input("Digite nome do produto: ")).strip()
+            #print("\n Ou digite 'sair' para voltar ao menu.")
             if not produto:
                 raise ValueError("Nome do produto não pode ser vazio.")
             if produto.lower() == 'sair':
@@ -66,7 +68,8 @@ def atualiza_quantidade(estoque: dict) -> None:
     produto = input("Digite o nome do produto a atualizar: ").strip()
     if produto not in estoque:
         print(f"Produto '{produto}' não encontrado no estoque.")
-        return
+        return #Lembrnado que o return finaliza a execução do bloco.
+
 
     try:
         nova_quantidade = int(input("Digite a nova quantidade: "))
@@ -78,17 +81,19 @@ def atualiza_quantidade(estoque: dict) -> None:
     except ValueError as e:
         print(f"Erro: {e}")
 
-        return
-
 
 def remove_produto(estoque:dict) -> None:
     print("Selecionou excluir produto...")
-    chave = str(input("Digite o nome do produto: "))
-    if chave not in estoque:
-        print(f"Chave {chave} não encontrada no {estoque}")
+    produto = str(input("Digite o nome do produto: ")).strip()
+
+
+    if produto not in estoque:
+        print(f"Chave {produto} não encontrada no {estoque}")
         return
-    estoque.pop(chave)
-    print(f"Chave {chave} removida do estoque.")
+
+
+    estoque.pop(produto)
+    print(f"Chave {produto} removida do estoque.")
     print(f"Estoque atualizado: {estoque}")
 
 
@@ -102,22 +107,26 @@ def main():
             print("3 - Remover produto")
             print("4 - Sair")
             opcao = int(input("Digite opção desejada: "))
-            if opcao not in [1,2,3,4]:
-                raise ValueError("Digite opção válida.")
             if opcao == 1:
                 estoque_dict(estoque)
-                return main()
-            if opcao == 2:
+
+            elif opcao == 2:
                 atualiza_quantidade(estoque)
-                return main()
-            if opcao == 3:
+
+            elif opcao == 3:
                 remove_produto(estoque)
-                return main()
-            if opcao == 4:
+
+            elif opcao == 4:
                 raise SystemExit("Saindo do programa...")
-            break
+
+            else:
+                print("Opção inserida é inválida. Tente novamente.")
+
+
 
         except KeyboardInterrupt:
-            raise SystemExit
+            raise SystemExit("Saindo do programa...")
+
+
 if __name__ == "__main__":
     main()
