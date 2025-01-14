1d2a49f79f6d9e458c1b04d5273f6e2cab3b8602
Lumi <drakko_games@outlook.com>
2025-01-14 14:24:47 -0300

    Create pointer.py

---
+++ PythonStudy_01/PointerAndElse/pointer.py
@@ -0,0 +1,12 @@
+a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))
+if a > b:
+    print("a is greater than b")
+elif a == b:
+    print("b is equal to a")
+else:
+    print("b is greater than a")
+#The assignment operator is the concept of declaring a value for a variable.
+#In essence, it assigns a value to the now-variable and everytime this variable
+#is referenced in the memory, this value will be bound to it till it's deleted
+#or till it is re-declared by any possible means.
