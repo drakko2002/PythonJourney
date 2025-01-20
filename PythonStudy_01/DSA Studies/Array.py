9b9ac41fb4221660a63acb7c096211a57f6224c2
Lumi <drakko_games@outlook.com>
2025-01-20 19:21:42 -0300

    Update Array.py

--- PythonStudy_01/DSA Studies/Array.py
+++ PythonStudy_01/DSA Studies/Array.py
@@ -1,5 +1,6 @@
 stock_prices_day = ['january','february','march','april','may','june','july','august'
     ,'september','october','november','december']
 stock_prices_prices = [20,30,40,50,60,70,80,90,100,110,120]
-dict_stock = dict(zip(stock_prices_day,stock_prices_prices))
-print(f"Price in first quarter was: \n{dict_stock}\n")
+dict_stock = list(zip(stock_prices_day,stock_prices_prices))
+for price,month in dict_stock
+    print("Price in first quarter was: ",dict_stock.index)
