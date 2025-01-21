4e9a3f30bf577ab0741dc1d326b6fcb2e47866e4
Lumi <drakko_games@outlook.com>
2025-01-21 20:17:10 -0300

    Update Array.py

--- PythonStudy_01/DSA Studies/Array.py
+++ PythonStudy_01/DSA Studies/Array.py
@@ -2,5 +2,7 @@ stock_prices_day = ['january','february','march','april','may','june','july','au
     ,'september','october','november','december']
 stock_prices_prices = [20,30,40,50,60,70,80,90,100,110,120]
 dict_stock = list(zip(stock_prices_day,stock_prices_prices))
-for price,month in dict_stock
-    print("Price in first quarter was: ",dict_stock.index)
+print(dict_stock[1])
+#Retorna o valor referente à mês/preço na segunda posição indexada da lista.
+#A primeira posição indexada por padrão é sempre 0.
+print(f"Price in first month of the year was: {dict_stock[0]}")
