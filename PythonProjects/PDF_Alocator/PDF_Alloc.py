eb6a741387fcdabf4065dfde6da67d36a713c450
Lumi <drakko_games@outlook.com>
2025-04-04 11:04:12 -0300

    Create PDF_Alloc.py

---
+++ PythonProjects/PDF_Alocator/PDF_Alloc.py
@@ -0,0 +1,25 @@
+import os
+import shutil
+
+downloads_path = os.path.join(os.path.expanduser("~", "Downloads"))
+pdfs_path = os.path.join(downloads_path,"PDFs")
+#Nisso, buscamos o caminho de sistema dos downloads e
+#definimos uma pasta para mover os arquivos de PDF
+
+os.makedirs(pdfs_path, exist_ok=True)
+#Faz checagem do diretório com validação por valor booleano
+#Para constatar se existe ou não.
+
+for filename in os.listdir(downloads_path):
+    if filename.lower().endswith(".pdf"):
+        origem = os.path.join(downloads_path, filename)
+        destino = os.path.join(pdfs_path, filename)
+        #Laço for pra definição de variáveis e caminhos
+
+        try:
+            shutil.move(origem, destino)
+            print(f"Movido com sucesso: {filename}")
+        except Exception as e:
+            print(f"Ocorreu um erro ao mover {filename}: {e}")
+            #Bloco try/except pra tentativa com retorno de exceção
+            #Salva na variável "e" para boas práticas.
