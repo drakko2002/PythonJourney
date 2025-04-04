a877469b585f6536a5e52605fff7b175a26c19df
Lumi <drakko_games@outlook.com>
2025-04-04 12:55:11 -0300

    Modified JavaAndPy_Alloc
    - The program now moves .php files aswell.

--- PythonProjects/PDF_Alocator/ScriptsCT.py
+++ PythonProjects/PDF_Alocator/ScriptsCT.py
@@ -2,15 +2,15 @@ import os
 import shutil
 
 downloads_path = os.path.join(os.path.expanduser("~"), "Downloads")
-provisory_path = os.path.join(downloads_path, "Programming Files")
+ctengine = os.path.join(downloads_path, "CT Script Files")
 
-os.makedirs(provisory_path, exist_ok=True)
+os.makedirs(ctengine, exist_ok=True)
 
-def javapy():
+def cten():
     for filename in os.listdir(downloads_path):
-        if filename.lower().endswith(".java") or filename.lower().endswith(".py") or filename.lower().endswith(".php"):
+        if filename.lower().endswith(".ct"):
             origem = os.path.join(downloads_path, filename)
-            destino = os.path.join(provisory_path, filename)
+            destino = os.path.join(ctengine, filename)
 
 
             try:
@@ -18,4 +18,4 @@ def javapy():
                 print(f"O arquivo {filename} foi movido com sucesso para {origem}!")
             except Exception as e:
                 print(f"Ocorreu um erro ao mover {filename} para {destino}: {e}")
-javapy()
+cten()
